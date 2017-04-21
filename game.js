e1d5cf132d090000418f53549f93b25d2c70a231
Evan Rothstein <evanhrothstein@gmail.com>
2017-04-21 19:47:51 -0400

    Adding game to github

---
+++ game.js
@@ -0,0 +1,58 @@
+let snake;
+let food;
+
+let gridScale = 20;
+
+function setup() {
+  createCanvas(500, 500);
+  frameRate(10);
+
+  snake = new Snake();
+  positionFood();
+}
+
+function positionFood(){
+  food = createRandomVector();
+  food.mult(gridScale);
+}
+
+function createRandomVector() {
+  return createVector(getRandomCol(),getRandomRow());
+}
+
+function getRandomCol() {
+  return floor(random(floor(height) / gridScale));
+}
+
+function getRandomRow() {
+  return floor(random(floor(width) / gridScale));
+}
+
+function draw() {
+  background(52);
+
+  if(snake.checkIsEat(food)) {
+    positionFood();
+  }
+
+  snake.update();
+  snake.show();
+
+  fill(255, 00, 100);
+  rect(food.x, food.y, gridScale, gridScale);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    snake.direction(0, -1);
+  }
+  else if (keyCode === DOWN_ARROW) {
+    snake.direction(0, 1);
+  }
+  else if (keyCode === LEFT_ARROW) {
+    snake.direction(-1, 0);
+  }
+  else if (keyCode === RIGHT_ARROW) {
+    snake.direction(1, 0);
+  }
+}
---
+++ snake.js
@@ -0,0 +1,80 @@
+function Snake() {
+  const self = this;
+
+  self.head = Point(0,0);
+
+  self.tail = [];
+
+  self.xSpeed = 1;
+  self.ySpeed = 0;
+
+  self.update = update;
+
+  self.show = show;
+
+  self.direction = direction;
+
+  self.checkIsEat = checkIsEat;
+
+  function update() {
+    self.tail.map(function (item, index, collection){
+      let nextItem = collection[index + 1];
+      item.x = nextItem ? nextItem.x : self.head.x;
+      item.y = nextItem ? nextItem.y : self.head.y;
+    })
+
+    self.head.x = self.head.x + (self.xSpeed * gridScale);
+    self.head.y = self.head.y + (self.ySpeed * gridScale);
+
+    self.head.x = constrain(self.head.x, 0, width - gridScale)
+    self.head.y = constrain(self.head.y, 0, height - gridScale)
+
+    checkForDeath()
+  }
+
+  function checkForDeath() {
+    self.tail.forEach(function (point){
+      if (getDistanceToHead(point) < 1) {
+        //reset
+        self.tail = []
+        self.head = Point(0,0)
+      }
+    })
+  }
+
+  function show() {
+    fill(255);
+    rect(self.head.x, self.head.y, gridScale, gridScale);
+    self.tail.forEach(function (point){
+      rect(point.x, point.y, gridScale, gridScale);
+    })
+  }
+
+  function direction(x, y) {
+    self.xSpeed = x;
+    self.ySpeed = y;
+  }
+
+  function checkIsEat(food) {
+    if (getDistanceToHead(food) < 1){
+      self.tail.push(Point(self.head.x, self.head.y));
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  function getDistanceToHead(food) {
+    return dist(self.head.x, self.head.y, food.x, food.y)
+  }
+
+}
+
+function Point(x,y) {
+  return {
+    x:x,
+    y:y,
+  };
+}
+
